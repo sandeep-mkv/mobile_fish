d2711091e32fc55a9f4e2b8f9479ce24b3429108
Sandeep Venugopal <sandeep.mkv@gmail.com>
2016-09-12 00:12:37 +0530

    Move phone_number_matcher class into lib/gem folder and use gem namespace

--- test/phone_number_matcher_test.rb
+++ test/phone_number_matcher_test.rb
@@ -1,53 +1,52 @@
 require 'test_helper'
-require "phone_number_matcher"
+require "mobile_fish/phone_number_matcher"
 
 DEFAULT_DICTIONARY_FILE = "#{Dir.pwd}/test/data/default_dictionary.txt"
 
 class PhoneNumberMatcherTest < Minitest::Test
 
+  def setup
+    @phone_number_matcher = MobileFish::PhoneNumberMatcher.new(DEFAULT_DICTIONARY_FILE)
+  end
+
   def test_that_it_cannot_be_initialized_without_a_dictionary_file
-    assert_raises(ArgumentError) { PhoneNumberMatcher.new }
+    assert_raises(ArgumentError) { MobileFish::PhoneNumberMatcher.new }
   end
 
   def test_that_it_can_take_an_optional_phone_number_file_on_initialize
-    phone_number_matcher1 = PhoneNumberMatcher.new(DEFAULT_DICTIONARY_FILE)
+    phone_number_matcher1 = MobileFish::PhoneNumberMatcher.new(DEFAULT_DICTIONARY_FILE)
     assert phone_number_matcher1
-    phone_number_matcher2 = PhoneNumberMatcher.new(DEFAULT_DICTIONARY_FILE, "file")
+    phone_number_matcher2 = MobileFish::PhoneNumberMatcher.new(DEFAULT_DICTIONARY_FILE, "file")
     assert phone_number_matcher2
   end
 
   def test_that_it_gives_the_word_matches_for_given_phone_number
-    phone_number_matcher = PhoneNumberMatcher.new(DEFAULT_DICTIONARY_FILE)
-    words = phone_number_matcher.word_matches "7829"
+    words = @phone_number_matcher.word_matches "7829"
     assert_instance_of Array, words
     assert_equal "RUBY", words.first
   end
 
   def test_that_it_gives_the_word_matches_for_given_phone_number_having_whitespace_and_punctuation
-    phone_number_matcher = PhoneNumberMatcher.new(DEFAULT_DICTIONARY_FILE)
-    words = phone_number_matcher.word_matches "4692739 6,8"
+    words = @phone_number_matcher.word_matches "4692739 6,8"
     assert_instance_of Array, words
     assert_equal "HOW-ARE-YOU", words.first
   end
 
   def test_that_it_gives_correct_word_matches_for_a_given_phone_number_where_result_has_both_word_and_digit
-    phone_number_matcher = PhoneNumberMatcher.new(DEFAULT_DICTIONARY_FILE)
-    words = phone_number_matcher.word_matches "1469273968"
+    words = @phone_number_matcher.word_matches "1469273968"
     assert_instance_of Array, words
     assert_equal "1-HOW-ARE-YOU", words.first
   end
 
   def test_that_it_does_not_give_a_match_if_two_consecutive_numbers_cant_be_replaced
-    phone_number_matcher = PhoneNumberMatcher.new(DEFAULT_DICTIONARY_FILE)
-    words = phone_number_matcher.word_matches "22551163"
+    words = @phone_number_matcher.word_matches "22551163"
     assert_instance_of Array, words
     assert_empty words
   end
 
   def test_that_it_gives_all_possible_word_matches_for_a_given_number_case
     ("0".."9").each do |num|
-      phone_number_matcher = PhoneNumberMatcher.new(DEFAULT_DICTIONARY_FILE)
-      words = phone_number_matcher.word_matches "#{num}225592255"
+      words = @phone_number_matcher.word_matches "#{num}225592255"
       assert_instance_of Array, words
       assert_equal 4, words.size
       assert words.include? "#{num}-BALL-9-BALL"
